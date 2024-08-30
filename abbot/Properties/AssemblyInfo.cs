21de490f51dda85d91c9710a4e8cdb578d774bd0
Tim Baxendale <pcluddite@outlook.com>
2024-08-29 20:36:51 -0500

    Upgrade abbot.csproj

--- abbot/Properties/AssemblyInfo.cs
+++ abbot/Properties/AssemblyInfo.cs
@@ -6,16 +6,6 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("abbot")]
-[assembly: AssemblyDescription("Command line tool for Trithemius")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("abbot")]
-[assembly: AssemblyCopyright("Copyright © 2024 Timothy Baxendale")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -26,16 +16,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("e5ce946c-cc97-4b1e-8f84-472878c6d167")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.5.1.0")]
-[assembly: AssemblyFileVersion("0.5.1.0")]
